4ee8d83d463303e6e351322be3e27a698b5993d7
Moi6-23 <77814787+Moi6-23@users.noreply.github.com>
2024-03-23 09:50:51 -0500

    Se elimina la whitelist

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const port = 3000;
 
 app.use(express.json());
 
-const whiteList = [
-  'http://127.0.0.1:5500',
-]
+// const whiteList = [
+//   'http://127.0.0.1:5500',
+// ]
 
-const options = {
-  origin: (origin, callback) => {
-    if(whiteList.includes(origin)){
-      callback(null, true);
-    }else{
-      callback(new Error('Domain not allowed'))
-    }
-  } 
-}
+// const options = {
+//   origin: (origin, callback) => {
+//     if(whiteList.includes(origin)){
+//       callback(null, true);
+//     }else{
+//       callback(new Error('Domain not allowed'))
+//     }
+//   } 
+// }
 
-app.use(cors(options))
+app.use(cors())
 
 
 app.listen(port, () => {
